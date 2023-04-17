08f486925c45861f70d93b5778643bd0adc09660
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2023-04-16 21:34:11 -0300

    Refatoração de código: simplifica acesso ao slice.

--- text/text.go
+++ text/text.go
@@ -9,5 +9,5 @@ var numbersRegex = regexp.MustCompilePOSIX("[0-9]+")
 
 func ExtractNumber(text string) string {
 	nums := numbersRegex.FindAllString(text, -1)
-	return strings.Join(nums[:], "")
+	return strings.Join(nums, "")
 }
