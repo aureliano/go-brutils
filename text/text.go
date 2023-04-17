147e6a83e8cfa93954f474a06dbed927306207e9
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2023-04-16 21:25:57 -0300

    Pacote para tratamento de texto. Extração de números de variável string com número mascarado.

---
+++ text/text.go
@@ -0,0 +1,13 @@
+package text
+
+import (
+	"regexp"
+	"strings"
+)
+
+var numbersRegex = regexp.MustCompilePOSIX("[0-9]+")
+
+func ExtractNumber(text string) string {
+	nums := numbersRegex.FindAllString(text, -1)
+	return strings.Join(nums[:], "")
+}
---
+++ text/text_test.go
@@ -0,0 +1,30 @@
+package text_test
+
+import (
+	"testing"
+
+	"github.com/aureliano/go-brutils/text"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExtractNumber(t *testing.T) {
+	type testCase struct {
+		name     string
+		input    string
+		expected string
+	}
+	testCases := []testCase{
+		{name: "empty string", input: "", expected: ""},
+		{name: "only text", input: "abcdef", expected: ""},
+		{name: "only number", input: "12345", expected: "12345"},
+		{name: "formatted CPF", input: "012.345.678-90", expected: "01234567890"},
+		{name: "formatted CNPJ", input: "01.234.567/0001-00", expected: "01234567000100"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := text.ExtractNumber(tc.input)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
