9e2702c5c0d2d363050d1c30d2702057df667c7f
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2023-04-17 17:21:36 -0300

    Documentação do pacote number.

---
+++ number/doc.go
@@ -0,0 +1,22 @@
+/*
+O pacote number é um pacote com funcções utilitárias para manipulação de números.
+
+# ExtrairNumeros
+
+Função para remoção de todo caractere que não for um número decimal da cadeia de texto.
+Pode ser usado pra remover máscaras de números cadastrais como CPF e CNPJ.
+
+	numeros := number.ExtrairNumeros("123.456.789-01")
+	fmt.Println(numeros) // Saída: 12345678901
+
+	numeros = number.ExtrairNumeros("12.345.678/0001-23")
+	fmt.Println(numeros) // Saída: 12345678000123
+
+# GerarUnidadeDecimal
+
+Função para geração aleatória de um número decimal entre zero e nove.
+
+	ud := GerarUnidadeDecimal()
+	fmt.Println(ud) // Saída: um número inteiro entre zero e nove (0-9).
+*/
+package number
