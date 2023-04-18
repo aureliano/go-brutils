93c3f15e27bd455b667732b748089dfc8481755e
Aureliano Caldeira Horta França <aureliano.franca@serpro.gov.br>
2023-04-18 16:37:56 -0300

    Completar número de CNPJ.

--- cmd/cnpj.go
+++ cmd/cnpj.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/aureliano/go-brutils/rfb"
 	"github.com/spf13/cobra"
@@ -22,6 +23,7 @@ func newCNPJCommand() *cobra.Command {
 	}
 
 	cmd.AddCommand(newGerarCNPJCommand())
+	cmd.AddCommand(newCompletarCNPJCommand())
 
 	return cmd
 }
@@ -43,12 +45,58 @@ func newGerarCNPJCommand() *cobra.Command {
 	return cmd
 }
 
+func newCompletarCNPJCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "completar",
+		Short: "Completar um número de CNPJ",
+		Long: "Completa um número de CNPJ, ora preenchendo com zeros a esquerda " +
+			"ora preenchendo os dígitos verificadores.",
+		Example: fmt.Sprintf(`  %s cpf completar 1981621`, project.binName),
+		Run: func(cmd *cobra.Command, args []string) {
+			completarNumeroCNPJ(cmd, args)
+		},
+	}
+
+	cmd.Flags().BoolP("formatar", "f", false, "Formatar a saída com a máscara ##.###.###/####-##")
+
+	return cmd
+}
+
 func gerarNumeroCNPJ(cmd *cobra.Command) {
 	formatar, _ := cmd.Flags().GetBool("formatar")
 	cnpj, err := rfb.GerarCNPJ()
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	if formatar {
+		fmt.Println(cnpj.Formatado())
+	} else {
+		fmt.Println(cnpj.Desformatado())
+	}
+}
+
+func completarNumeroCNPJ(cmd *cobra.Command, args []string) {
+	formatar, _ := cmd.Flags().GetBool("formatar")
+
+	if len(args) == 0 {
+		cmd.Help()
+		fmt.Println()
+		log.Fatalln("Esperava um número inteiro >= 0.")
+	} else if len(args) > 1 {
+		cmd.Help()
+		fmt.Println()
+		log.Fatalln("Esperava apenas um número de entrada.")
+	}
+
+	numero, err := strconv.Atoi(args[0])
+	if err != nil {
+		log.Fatalf("%s não é um número válido.\n", args[0])
+	} else if numero == 0 {
+		log.Fatalln("Esperava um número inteiro >= 0.")
+	}
+
+	cnpj := rfb.NewCNPJ(uint(numero))
 
 	if formatar {
 		fmt.Println(cnpj.Formatado())
